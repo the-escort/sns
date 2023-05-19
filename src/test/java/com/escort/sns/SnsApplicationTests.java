adbd85c452bf32a939e88bf64b766d868b8094b0
the-escort <vanitas613@naver.com>
2023-05-20 04:21:56 +0900

    Create sns project

---
+++ src/test/java/com/escort/sns/SnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escort.sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
